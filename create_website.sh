a947a8b9c504cd01d2378351cc8bfd1c17270587
Hyung-Gyu <hgryoo@cubrid.com>
2025-11-26 10:19:40 +0900

    add website

---
+++ create_website.sh
@@ -0,0 +1,4 @@
+/home/hgryoo/dev/ann-benchmarks/.venv/bin/python create_website.py --outputdir "websites/temp" --scatter --recompute
+
+cd websites/temp
+/home/hgryoo/dev/ann-benchmarks/.venv/bin/python -m http.server 8080
